e48c0708ec1647d5d47c144ed112068054ebf111
IzGabs <felipemcont@gmail.com>
2022-10-24 22:43:01 -0300

    Adicao do example

---
+++ Modules/shape_memorizer/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// This should be used only for debug purposes
+
+class ShapeMemorizerHome extends StatelessWidget {
+  const ShapeMemorizerHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Shape Memorizer Home'),
+      ),
+    );
+  }
+}
--- Modules/shape_memorizer/lib/shape_memorizer.dart
+++ Modules/shape_memorizer/lib/shape_memorizer.dart
@@ -1,7 +1,3 @@
 library shape_memorizer;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'main.dart';
